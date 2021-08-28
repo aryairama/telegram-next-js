3197d5ddab9771e8b769fa54c94d234ec3fc7f1c
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-28 10:20:46 +0700

    initial project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useStore } from '../redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { persistStore } from 'redux-persist';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const store = useStore(pageProps.initialReduxState);
+  const persistor = persistStore(store, {}, function () {
+    persistor.persist();
+  });
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NextNProgress color="#ffce65" startPosition={0.3} stopDelayMs={200} height={4} showOnShallow={true} />
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
