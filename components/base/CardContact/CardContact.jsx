2afdf1a52b48d7a34a81aca90eb5519da1b62e2c
Arya Irama Wahono <aryairama987@gmail.com>
2021-09-01 10:12:43 +0700

    fix break word card contacts

--- components/base/CardContact/CardContact.jsx
+++ components/base/CardContact/CardContact.jsx
@@ -5,15 +5,20 @@ const CardContact = (props) => {
       className="flex flex-row justify-between items-center w-full my-3 shadow rounded-l-2xl hover:shadow-lg font-Rubik"
     >
       <div className="flex items-center">
-        <img className="w-16 h-16 rounded-2xl" src={props.profile_img} alt="icon-profile" />
+        <img className="w-16 h-16 rounded-2xl object-contain" src={props.profile_img} alt="icon-profile" />
         <div className="flex flex-col pl-5">
-          <p className="font-bold text-lg">{props.name}</p>
-          <p className="text-gray-500">{props.phone_number ? props.phone_number : 'Not set'}</p>
+          <p className="font-bold text-base lg:text-lg break-all">{props.name}</p>
+          <p className="text-gray-500 break-all">{props.phone_number ? props.phone_number : 'Not set'}</p>
         </div>
       </div>
       <div className="flex flex-row">
         {props.type === 'add' && (
-          <img onClick={props.clickImg} className="w-5 h-5 mr-5" src="/assets/icon/plus-square-fill.svg" alt="plus-icon" />
+          <img
+            onClick={props.clickImg}
+            className="w-5 h-5 mr-5"
+            src="/assets/icon/plus-square-fill.svg"
+            alt="plus-icon"
+          />
         )}
         {props.type === 'delete' && (
           <img onClick={props.clickImg} className="w-5 h-5 mr-5" src="/assets/icon/trash-fill.svg" alt="plus-icon" />
