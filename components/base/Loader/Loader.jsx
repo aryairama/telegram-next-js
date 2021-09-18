393732b230bfcad4cfcaa4554cfa563f3ab65548
Arya Irama Wahono <aryairama987@gmail.com>
2021-09-18 22:56:05 +0700

    update z-index loader

--- components/base/Loader/Loader.jsx
+++ components/base/Loader/Loader.jsx
@@ -4,9 +4,10 @@ const Loader = (props) => {
   const { show } = useSelector((state) => state.loader);
   return (
     <div
+      style={{ zIndex: '100' }}
       className={`${
         props.show ? (props.show === true ? 'flex' : 'hidden') : show ? 'flex' : 'hidden'
-      } absolute w-full h-full right-0 left-0 top-0 bottom-0 bg-white z-50 flex justify-center items-center`}
+      } absolute w-full h-full right-0 left-0 top-0 bottom-0 bg-white flex justify-center items-center`}
     >
       <img src="/assets/icon/loader.gif" alt="icon-loader" className="h-64 w-64" />
     </div>
