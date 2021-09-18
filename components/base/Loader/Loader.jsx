bf4ca373f4c30ef163d0df6ceae6843c1fd51630
Arya Irama Wahono <aryairama987@gmail.com>
2021-09-18 16:36:38 +0700

    add loader on router next js

--- components/base/Loader/Loader.jsx
+++ components/base/Loader/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = (props) => {
   return (
     <div
       className={`${
-        show ? 'flex' : 'hidden'
+        props.show ? (props.show === true ? 'flex' : 'hidden') : show ? 'flex' : 'hidden'
       } absolute w-full h-full right-0 left-0 top-0 bottom-0 bg-white z-50 flex justify-center items-center`}
     >
       <img src="/assets/icon/loader.gif" alt="icon-loader" className="h-64 w-64" />
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import '../styles/globals.css';
 import { useStore } from '../redux/store';
 import { Provider } from 'react-redux';
@@ -5,8 +6,17 @@ import { PersistGate } from 'redux-persist/lib/integration/react';
 import { persistStore } from 'redux-persist';
 import NextNProgress from 'nextjs-progressbar';
 import { Loader } from '../components/base';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => setLoading(true));
+    router.events.on('routeChangeComplete', () => setLoading(false));
+    router.events.on('routeChangeError', () => setLoading(false));
+  }, [router]);
   const store = useStore(pageProps.initialReduxState);
   const persistor = persistStore(store, {}, function () {
     persistor.persist();
@@ -15,7 +25,7 @@ function MyApp({ Component, pageProps }) {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <NextNProgress color="#7E98DF" startPosition={0.3} stopDelayMs={200} height={4} showOnShallow={true} />
-        <Loader />
+        <Loader show={loading} />
         <Component {...pageProps} />
       </PersistGate>
     </Provider>
